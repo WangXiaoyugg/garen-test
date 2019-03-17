1d3d819bfe7f0e61da27fd80cdd04bfb128cc00a
GarenWang <2270099149@qq.com>
2019-03-17 17:56:15 +0800

    完成icon的测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,11 +1,17 @@
 import * as renderer from "react-test-renderer"
 import React from 'react'
 import Icon from '../icon'
-// import {mount} from 
+import {mount} from "enzyme"
 
 describe("test icon component", () => {
     it("render successfully", () => {
         const json = renderer.create(<Icon name="qq"/>).toJSON()
         expect(json).toMatchSnapshot()
+    })
+    it("icon onClick fn called", () => {
+        const fn = jest.fn()
+        const component = mount(<Icon name="qq" onClick={fn}/>)
+        component.find("svg").simulate("click")
+        expect(fn).toBeCalled()
     })
 })
