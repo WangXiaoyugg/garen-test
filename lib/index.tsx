0c68b800ba49571e9071b546dff6db029fa8078b
GarenWang <2270099149@qq.com>
2019-03-24 17:39:03 +0800

    修改index.tsx和package.json版本号

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e.target);
-}
-
-ReactDOM.render(
-    <div>
-        <Icon name="wechat"
-            onClick={fn}
-            onMouseEnter={() => console.log("onMouseEnter")}
-            onMouseLeave={() => console.log("onMouseLeave")}
-            onTouchStart={() => console.log("onTouchStart")}
-        />
-    </div>
-, document.querySelector("#root"));
+export {default as Icon} from './icon/icon'
