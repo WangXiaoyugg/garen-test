9ae555f3bd5ad0fce04f295e6b7ffca34e3c0d81
GarenWang <2270099149@qq.com>
2019-03-24 17:27:44 +0800

    修改gitignore

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     coverageDirectory: "coverage",
     coverageReporters: ['text', 'lcov'],
 
-    reporters: ["default"],
+    reporters: ["default", "jest-junit"],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
     globals: {
